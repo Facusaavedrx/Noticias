787205cac826016f563291ee735c3efe80383854
Facundo Saavedra <facusaavedrx@gmail.com>
2023-02-07 09:54:49 -0300

    Primer commit, se agrego componente Header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+
+function App () {
+  return (
+    <main className='App'>
+      <Header
+        titulo='Buscador de Noticias'
+      />
+      <article className='container white'>
+        <h2>Formulario</h2>
+      </article>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header ({ titulo }) {
+  return (
+    <nav>
+      <a href='#!' className='brand-logo center'> {titulo} </a>
+    </nav>
+  )
+}
+
+export default Header
