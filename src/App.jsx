788af10cf26e80de9d8a55f687ea0ece20846b37
Facundo Saavedra <facusaavedrx@gmail.com>
2023-02-07 10:48:19 -0300

    consultando a la API y leyendo los resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 
 function App () {
   const [categoria, guardarCategoria] = useState('')
+  const [noticias, guardarNoticias] = useState([])
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=51b85702c81a46b2971e64fe5bbad48c`
+      const respuesta = await fetch(url)
+      const noticias = await respuesta.json()
+      console.log(noticias.articles)
+    }
+    consultarApi()
+  }, [categoria])
   return (
     <main className='App'>
       <Header
